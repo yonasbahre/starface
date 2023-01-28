b685f01742fe96e02a5c59665a50e9f1ef67bd47
mhayworth <73895101+mhayworth@users.noreply.github.com>
2023-01-28 14:45:08 -0500

    initial commit for starserv

---
+++ starserv/src/main/java/SwampHacks23/StarData/StarDataApplication.java
@@ -0,0 +1,13 @@
+package SwampHacks23.StarData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarDataApplication.class, args);
+	}
+
+}
