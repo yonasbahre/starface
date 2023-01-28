648dabedd027d44595038d6e4fb5181742888df9
Yonas Bahre <yonasbahre7@gmail.com>
2023-01-28 16:32:47 -0500

    Add background image and new font

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,16 +3,38 @@ import styled from "styled-components"
 function App() {
   return (
     <AppDiv>
-      Welcome to Starface
+      <h1>
+        Welcome to Starface
+      </h1>
     </AppDiv>
   )
 }
 
+// Background URL
+const bgUrl: string = `https://www.nasa.gov/sites/default/files/styles/full_width_feature/public/thumbnails/image/main_image_star-forming_region_carina_nircam_final-5mb.jpg`;
+
 const AppDiv = styled.div`
-  font-family: Orbitron, sans-serif;
-  background: linear-gradient(150deg, rgb(20, 20, 20), rgb(20, 20, 120));
+  h1 {
+    font-family: Orbitron, sans-serif;
+  }
+
+  h1, h2, h3, h4, h5, h6 {
+    color: cyan;
+    margin: 0;
+    padding: 10px 0px 0px 0px;
+  }
+  
+  font-family: Josefin Sans, sans-serif;
+  background: linear-gradient(150deg, rgba(20, 20, 20, 0.85), rgba(20, 20, 120, 0.85)), url(${bgUrl});
+  background-repeat: no-repeat;
+  background-size: cover;
   color: white;
-  height: 100vh;
+  margin: 0;
+  padding: 0;
+  min-height: 100vh;
+  min-width: 100vh;
+  height: 100%;
+  width: 100%;
 `
 
 export default App
