2505ae9f3ba6280ddd1de650f14f797786ac2893
Yonas Bahre <yonasbahre7@gmail.com>
2023-01-29 04:24:52 -0500

    Add message board

--- client/src/starpage/index.tsx
+++ client/src/starpage/index.tsx
@@ -1,13 +1,43 @@
-import { useEffect, useContext } from "react";
+import { useState, useContext } from "react";
 import Star from "../shared/star";
 import { StarContext } from "../App";
 import Navbar from "./navbar";
 import styled from "styled-components";
-import Fact, { FactProps } from "./fact";
-import { Link } from "react-router-dom";
+import Fact from "./fact";
+import MessageScroll from "./messagescroll";
+import axios from "axios";
 
 const StarPage: React.FC<{}> = () => {
-  const { star } = useContext(StarContext);
+  const { star, updateStar } = useContext(StarContext);
+  const [formInput, setFormInput] = useState<string>(""); 
+
+  const handleChange = (e: any): void => {
+    setFormInput(e.target.value);
+  }
+
+  const handleSubmit = async (e: any): Promise<void> => {
+    e.preventDefault();
+    console.log(`Submitting: ${formInput}`);
+
+    try {
+      const resp = await axios.post(
+        `${import.meta.env.VITE_STARSERV}/star/${star.id}`,
+        {message: formInput},
+        {
+          headers: { 
+            'Content-Type' : 'application/json' 
+          }
+        }
+      );
+      updateStar(resp.data);
+      console.log(resp.data);
+      setFormInput("");
+    } catch (e: any) {
+      // TODO: Delete log
+      setFormInput("");
+      console.log(e);
+    }
+  }
 
   return (
     <OuterStarDiv>
@@ -25,7 +55,16 @@ const StarPage: React.FC<{}> = () => {
           <Fact label="Fun Fact!" info={star.fact} />
         </FactTable>
 
-        <h2>Message Board goes here</h2>
+        <MessageBox>
+          <MessageScroll messages={star.messages}/>
+          <FormStyle onSubmit={handleSubmit}>
+            <textarea placeholder="Say hello to your little friends!" value={formInput} onChange={handleChange} style={{minWidth: "50%"}}></textarea>
+            <div className="buttonDiv" onClick={handleSubmit}>
+              Post Message!
+            </div>
+          </FormStyle>
+        </MessageBox>
+
 
         <div style={{ display: "inline", margin: 10 }}>
           <a
@@ -58,8 +97,29 @@ const OuterStarDiv = styled.div`
 const StarDiv = styled.div`
   display: flex;
   flex-direction: column;
+  align-items: center;
 `;
 
 const FactTable = styled.div`
   padding-top: 0.5rem;
+`;
+
+const FormStyle = styled.form`
+  textarea {
+    background: rgba(20, 20, 40, 0.7);
+    border-style: none;
+    color: white;
+    font-family: Josefin Sans, sans-serif;
+    height: 5em;
+    width: inherit;
+    resize: none;
+  }
+
+  display: flex;
+  flex-direction: column;
+`;
+
+const MessageBox = styled.div`
+  min-width: 50%;
+  max-width: 80%;
 `;
---
+++ client/src/starpage/messagescroll.tsx
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+
+export type ScrollProps = {
+  messages: string[];
+}
+
+type MessageProps = {
+  message: string;
+}
+
+const Message: React.FC<MessageProps> = ({message}) => {
+  return (
+    <MessageDiv>
+      {message}
+    </MessageDiv>
+  );
+}
+
+const MessageScroll: React.FC<ScrollProps> = ({messages}) => {
+  return (
+    <ScrollDiv>
+      {messages.length === 0
+      ?
+      `No messages yet - be the first to plant your flag!`
+      :
+      messages.map((msg) => <Message message={msg}/>)
+      }
+    </ScrollDiv>
+  );
+}
+
+export default MessageScroll;
+
+const ScrollDiv = styled.div`
+  min-width: 50%;
+  align-self: center;
+  border-style: solid;
+  border-radius: 6px;
+  border-width: 2px;
+  display: flex;
+  flex-direction: column;
+  overflow-y: scroll;
+  text-align: left;
+  max-height: 8rem;
+`;
+
+const MessageDiv = styled.div`
+  padding: 10px;
+  font-size: 1em;
+`
