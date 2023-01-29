a2fc55d75f32f7831b4daae8ce66b8fc6da4b88c
Yonas Bahre <yonasbahre7@gmail.com>
2023-01-28 23:10:29 -0500

    Add getting starId from faceserv

--- client/src/home/index.tsx
+++ client/src/home/index.tsx
@@ -1,8 +1,42 @@
+import axios from "axios";
+import { useState } from "react";
 import styled from "styled-components";
 
 const Home: React.FC<{}> = () => {
-  const uploadImage = (): void => {
-    console.log(`Uploading image!`);
+  const [image, setImage] = useState();
+  const [emptyUpload, setEmptyUpload] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [uploadErr, setUploadErr] = useState<boolean>(false);
+
+  const handleChange = (e: any): void => {
+    setImage(e.target.files[0]);
+  }
+
+  const uploadImage = async (): Promise<void> => {
+    if (!image) {
+      setEmptyUpload(true);
+      return;
+    }
+
+    setEmptyUpload(false);
+    console.log(`Uploading!`);
+    try {
+      setUploadErr(false);
+      setLoading(true);
+      const formData: FormData = new FormData();
+      formData.append("file", image);
+      const resp = await axios.post(`${import.meta.env.VITE_FACESERV}/faceService/getID`, formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      });
+      const starId = resp.data.star_id;
+    } catch (e: any) {
+      setLoading(false);
+      setUploadErr(true);
+      console.log(e);
+      // TODO: set error
+    }
   }
   
   return (
@@ -11,18 +45,46 @@ const Home: React.FC<{}> = () => {
         starface
       </h1>
 
-      <h2 className="buttonDiv" onClick={uploadImage}>
-        Upload Image!
-      </h2>
+      {loading ?
+        <div>Loading...</div>
+      :
+        <form style={{display: "flex", flexDirection: "column"}}>
+          <div style={{display: "inline", paddingBottom: "1rem", textAlignLast: "center"}}>
+            <input id="file-upload" type="file" onChange={handleChange}/>
+          </div>
+
+          {emptyUpload ?
+            <div style={{paddingBottom: 5}}>
+              Please choose an image to upload.
+            </div>
+            :
+            <div></div>
+          }
+
+          {uploadErr ?
+            <div style={{paddingBottom: 5}}>
+              Error uploading image. Please try again.
+            </div>
+            :
+            <div></div>
+          }
+
+          <div style={{display: "inline"}}>
+            <h2 className="buttonDiv" onClick={uploadImage}>
+              Upload Image!
+            </h2>
+          </div>
+        </form> 
+      }
       
       <p style={{fontSize: "1.5em"}}>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
-      nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit 
-      in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-      Excepteur sint occaecat cupidatat non proident, 
-      sunt in culpa qui officia deserunt mollit anim id est laborum.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit 
+        in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+        Excepteur sint occaecat cupidatat non proident, 
+        sunt in culpa qui officia deserunt mollit anim id est laborum.
       </p>
     </HomeDiv>
   );
@@ -33,4 +95,4 @@ export default Home;
 const HomeDiv = styled.div`
   width: 90%;
   text-align: center;
-`
+`;
