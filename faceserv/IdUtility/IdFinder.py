362ed3fc5cd7e3a89e21f1e1b1f5996f2b0f3b1e
Param Gupta <param.gupta107@gmail.com>
2023-01-29 04:48:24 -0500

    generate id from pca score

--- faceserv/IdUtility/IdFinder.py
+++ faceserv/IdUtility/IdFinder.py
@@ -3,4 +3,11 @@ from PCAUtility import PCAScore
 def findID(img):
     embedding = embeddingGenerator.getEmbedding(img)
     score = PCAScore.generateScore(embedding)
-    return 0
+    numStars = 10
+    # minVal = -0.385
+    # maxVal = 0.502
+    minVal = -0.25
+    maxVal = 0.25
+    id = round((((score - minVal) * (numStars-1 - 0)) / (maxVal - minVal)) + 0)
+    id = max(0, min(numStars-1, id))
+    return id
--- faceserv/PCAUtility/PCATrainer.py
+++ faceserv/PCAUtility/PCATrainer.py
@@ -10,9 +10,9 @@ def trainPCA():
     count = 0
     fileCount = len(os.listdir(directory))
     for idx, filename in enumerate(os.listdir(directory)):
-        print(str(idx) + "/" + str(fileCount), end="\r")
-        if count >= 5:
-            break
+        print(str(idx) + "/" + str(fileCount))
+        # if count >= 2000:
+        #     break
         try:
             f = os.path.join(directory, filename)
             if os.path.isfile(f):
@@ -20,10 +20,15 @@ def trainPCA():
                 embeddings.append(embedding)
                 count += 1
         except Exception:
+            print("skipped------")
             pass
     print()
     embeddings = np.array(embeddings) 
     print(embeddings.shape)
     pca = PCA(n_components = 1)
     scores = pca.fit_transform(embeddings)
+    print("min val", scores.min())
+    print("max val", scores.max())
+    print("mean val", scores.mean())
+    print("std val", scores.std())
     pk.dump(pca, open("pca.pkl","wb"))
