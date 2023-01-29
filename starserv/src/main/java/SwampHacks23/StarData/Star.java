3e9b4c9d867a752fd7f1cad8b537b28b6e919d34
mhayworth <73895101+mhayworth@users.noreply.github.com>
2023-01-28 22:16:31 -0500

    Add initialization of data and resources

--- starserv/src/main/java/SwampHacks23/StarData/Star.java
+++ starserv/src/main/java/SwampHacks23/StarData/Star.java
@@ -1,17 +1,18 @@
 package SwampHacks23.StarData;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Star {
-    public int id;
+    public final int id;
     public String name;
     public String constellation;
     public int distanceLightYears;
     public double solarMasses;
     public int temperature;
     public String fact;
-
-    public ArrayList<String> messages;
+    public String link;
+    public final List<String> messages = new ArrayList<String>();
 
     public Star(int id,
                 String name,
@@ -19,7 +20,8 @@ public class Star {
                 int distanceLightYears,
                 double solarMasses,
                 int temperature,
-                String fact) {
+                String fact,
+                String link) {
         this.id = id;
         this.name = name;
         this.constellation = constellation;
@@ -27,12 +29,44 @@ public class Star {
         this.solarMasses = solarMasses;
         this.temperature = temperature;
         this.fact = fact;
-        this.messages = new ArrayList<String>();
+        this.link = link;
     }
 
-    public Star AddMessage(String message) {
+    public void addMessage(String message) {
         messages.add(message);
-        return this;
+    }
+
+    public String getStarResource() {
+        if (temperature < 3000) {
+            return "red";
+        }
+        else if (temperature < 4000) {
+            return "orange";
+        }
+        else if (temperature < 8000) {
+            return "yellow";
+        }
+        else if (temperature < 15000) {
+            return "white";
+        }
+        else {
+            return "blue";
+        }
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "Star{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", constellation='" + constellation + '\'' +
+                ", distanceLightYears=" + distanceLightYears +
+                ", solarMasses=" + solarMasses +
+                ", temperature=" + temperature +
+                ", fact='" + fact + '\'' +
+                ", link='" + link + '\'' +
+                ", messages=" + messages +
+                '}';
     }
 }
 
